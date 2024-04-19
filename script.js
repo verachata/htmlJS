43b26b6ef87febdd024a6ec894bf320a24091ef7
verachata <chatalbashyanv@list.ru>
2024-04-19 13:47:06 +0400

    html and js

---
+++ script.js
@@ -0,0 +1,20 @@
+alert();
+function f(){
+    return 5+9 *2
+};
+
+alert();
+function f(){
+    return 5+9 *2
+};
+
+alert();
+function f(){
+    return 5+9 *2
+};
+
+alert();
+function f(){
+    return 5+9 *2
+};
+
